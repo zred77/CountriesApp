dae75a749b45607d7566c9525b4e941a8709ced5
Zoltan Veres <zred77@gmail.com>
2019-08-12 19:58:31 +0200

    Add model

---
+++ app/src/main/java/com/veresz/countries/model/RegionalBloc.kt
@@ -0,0 +1,8 @@
+package com.veresz.countries.model
+
+data class RegionalBloc(
+    val acronym: String,
+    val name: String,
+    val otherAcronyms: List<String>,
+    val otherNames: List<String>
+)
